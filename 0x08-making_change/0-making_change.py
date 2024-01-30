79a0f811c3a2fa6c18d691c65bfa7b80ab80dfa4
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2024-01-30 08:58:25 -0800

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+"""Making Change"""
 
 
-def make_change(coins, total):
-    """Determines the fewest number of coins needed \
-        to meet a given amount total"""
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed
+    to meet a given amount total"""
     if total <= 0:
         return 0
-
-    current_total = 0
-    used_coins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
-            return used_coins
-    return -1
+    remind = total
+    coin_needed = 0
+    coin_indexes = 0
+    coins_sorted = sorted(coins, reverse=True)
+    listlen = len(coins)
+    while remind > 0:
+        if coin_indexes >= listlen:
+            return -1
+        if remind - coins_sorted[coin_indexes] >= 0:
+            remind -= coins_sorted[coin_indexes]
+            coin_needed += 1
+        else:
+            coin_indexes += 1
+    return coin_needed
